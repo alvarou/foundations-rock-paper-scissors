e680efd21b8ef18fb9051157db9967e13d330530
AU <urrutia.alvaro@gmail.com>
2022-07-26 08:00:56 -0500

    Add playround and game functions
    Prompts for player selection for 5 rounds.
    Returns result to console for each round

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,50 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     return ['Rock', 'Paper', 'Scissors'][randomNumber];
 }
+
+//Write a function that plays a single round of Rock Paper Scissors. 
+//The function should take two parameters - the playerSelection and computerSelection - 
+//and then return a string that declares the winner of the round 
+//like so: "You Lose! Paper beats Rock"
+//create a function called playRound that will play a single round of Rock Paper Scissors
+// make function’s playerSelection parameter case-insensitive 
+//(so users can input rock, ROCK, RocK or any other variation
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return "You Lose! Scissors beats Paper";
+        } else {
+            return "You Win! Paper beats Rock";
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "You Win! Scissors beats Paper";
+        }
+    }
+}
+
+
+//play a game of Rock Paper Scissors with the user
+//create a function called playGame that will play a game of Rock Paper Scissors
+//plays 5 rounds of Rock Paper Scissors
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper, or Scissors?");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+//call the function playGame
+game();
